76f965e60c7586f25fba20a5a1f96f44acc73565
ENUGU VISHWANTH REDDY <93393384+Vishwanth123@users.noreply.github.com>
2022-06-18 15:32:07 +0530

    changed

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -7,28 +7,71 @@ import image3 from '../images/image3.png'
 function Aboutus(){
     return(
         <div>
-            <hr />
-            <div className="container">
-                <h1 className="text-center">Our Victories</h1>
-                <div className="row">
-                    <div className="col">
-                        <div className="card shadow p-3">
-                            <img src={image1} alt="image not found" className='w-100'/>
-                            <p>On April 30, BMRCL invited objections against 1,026 trees that need to be axed for Namma Metro Phase 2A. Since most citizens could not focus on assessing the impacts, Jhatkaa.org launched a petition asking Government authorities to postpone the deadline until lockdown is lifted. After emails, calls from many Jhakaa.org members, this deadline has been postponed to 10 days after the lockdown is lifted. Our collective effort helped us preserve more than a thousand trees in Bangalore.</p>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card shadow p-3">
-                            <img src={image2} alt="image not found" className='w-100'/>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card shadow p-3">
-                            <img src={image3} alt="image not found" className='w-100'/>
+            <div className='mt-3'>
+            <h1 className="text-center">Our Victories</h1>
+            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4 mt-5 w-100 gap-5  justify-content-evenly'>
+            {/* <h1 className="text-center">Our Victories</h1> */}
+                <div className='col p-1'>
+                    <div className='card card-body w-100 h-100 m-3 mx-auto shadow'>
+                        <img className=''src={image1} alt="" height="319px"/>
+                        <div className="text-center mt-4">
+                           <p>On April 30, BMRCL invited objections against 1,026 trees that need to be axed for Namma Metro Phase 2A. Since most citizens could not focus on assessing the impacts, Jhatkaa.org launched a petition asking Government authorities to postpone the deadline until lockdown is lifted. After emails, calls from many Jhakaa.org members, this deadline has been postponed to 10 days after the lockdown is lifted. Our collective effort helped us preserve more than a thousand trees in Bangalore.</p>
                         </div>
                     </div>
                 </div>
+                <div className='col p-1'>
+                    <div className='card card-body w-100 h-100 m-3 mx-auto shadow'>
+                        <img className=''src={image2} alt=""height="319px"/>
+                        <div className="text-center mt-4">
+                           <p>On April 30, BMRCL invited objections against 1,026 trees that need to be axed for Namma Metro Phase 2A. Since most citizens could not focus on assessing the impacts, Jhatkaa.org launched a petition asking Government authorities to postpone the deadline until lockdown is lifted. After emails, calls from many Jhakaa.org members, this deadline has been postponed to 10 days after the lockdown is lifted. Our collective effort helped us preserve more than a thousand trees in Bangalore.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col p-1'>
+                
+                <div className='card card-body w-100 h-100 m-3 mx-auto shadow'>
+                    <img className='' src={image3}  alt=""height="319px"/>
+                    <div className="text-center mt-4">
+                        <p>Stand Against the illegal two-finger test!</p>
+                    </div>
+                </div>
             </div>
+            </div>
+        </div>
+        <div className='mt-3'>
+            <h1 className="text-center">Our Campaigners</h1>
+            <div className='row row-cols-1 row-cols-sm-2 row-cols-md-4 mt-5 w-100 gap-5  justify-content-evenly'>
+            {/* <h1 className="text-center">Our Victories</h1> */}
+                <div className='col p-1'>
+                    <div className='card card-body w-100 h-100 m-3 mx-auto shadow'>
+                        <img className=''src={image1} alt="" height="319px"/>
+                        <div className="text-center mt-4">
+                           <p>On April 30, BMRCL invited objections against 1,026 trees that need to be axed for Namma Metro Phase 2A. Since most citizens could not focus on assessing the impacts, Jhatkaa.org launched a petition asking Government authorities to postpone the deadline until lockdown is lifted. After emails, calls from many Jhakaa.org members, this deadline has been postponed to 10 days after the lockdown is lifted. Our collective effort helped us preserve more than a thousand trees in Bangalore.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col p-1'>
+                    <div className='card card-body w-100 h-100 m-3 mx-auto shadow'>
+                        <img className=''src={image2} alt=""height="319px"/>
+                        <div className="text-center mt-4">
+                           <p>On April 30, BMRCL invited objections against 1,026 trees that need to be axed for Namma Metro Phase 2A. Since most citizens could not focus on assessing the impacts, Jhatkaa.org launched a petition asking Government authorities to postpone the deadline until lockdown is lifted. After emails, calls from many Jhakaa.org members, this deadline has been postponed to 10 days after the lockdown is lifted. Our collective effort helped us preserve more than a thousand trees in Bangalore.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col p-1'>
+                
+                <div className='card card-body w-100 h-100 m-3 mx-auto shadow'>
+                    <img className='' src={image3}  alt=""height="319px"/>
+                    <div className="text-center mt-4">
+                        <p>Stand Against the illegal two-finger test!</p>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+            
+       
+        </div>
         </div>
     )
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Carousel} from 'react-responsive-carousel'
+// import {Carousel} from 'react-responsive-carousel'
 import "bootstrap"
 function Home(){
     return(
         <div className='text-center container'>
             <h1 className="mt-5">Jhatkaa</h1>
-            <Carousel infiniteLoop autoPlay className='w-75 mt-5 mx-auto'>
+            {/* <Carousel infiniteLoop autoPlay className='w-75 mt-5 mx-auto'> */}
           <div className='image w-100'>
             <img className="w-100" src="https://i.ytimg.com/vi/kalp_iT6Zs4/maxresdefault.jpg"alt='' />
           </div>
@@ -15,7 +15,7 @@ function Home(){
           <div className='image w-100'>
             <img src="https://static.wixstatic.com/media/3290ed_4365025026fd4687a63a6569bb0300cb~mv2.jpg/v1/fill/w_1000,h_666,al_c,q_90,usm_0.66_1.00_0.01/3290ed_4365025026fd4687a63a6569bb0300cb~mv2.jpg" alt='' />
           </div>
-        </Carousel>
+        {/* </Carousel> */}
            
         </div>
     )
