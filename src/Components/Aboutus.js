43cca6792a1207510c372f16c9e254706ba0d4e9
ENUGU VISHWANTH REDDY <93393384+Vishwanth123@users.noreply.github.com>
2022-06-18 13:21:04 +0530

    about us

--- src/Components/Aboutus.js
+++ src/Components/Aboutus.js
@@ -1,7 +1,25 @@
+import react from 'react' 
+import image1 from '../images/image1.png'
+import image2 from '../images/image2.png'
+import image3 from '../images/image3.png'
+
+
 function Aboutus(){
     return(
         <div>
-             <h1>HI</h1>
+            <div className="card shadow">
+                <div className="row">
+                    <div className="col">
+                        <img src={image1} alt="image not found" className='w-50'/>
+                    </div>
+                    <div className="col">
+                        <img src={image2} alt="image not found" className='w-50'/>
+                    </div>
+                    <div className="col">
+                        <img src={image3} alt="image not found" className='w-50' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
